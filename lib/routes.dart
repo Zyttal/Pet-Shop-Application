c5983aa02c7a6cc253f353ffbb048d70513eefe3
Zyttal <calz483k@gmail.com>
2024-02-18 03:52:28 +0800

    Added Routes and Initialized Pages

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pet_shop_application/pages/cart_page.dart';
+import 'package:pet_shop_application/pages/details_page.dart';
+import 'package:pet_shop_application/pages/store_page.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return {
+    '/': (context) => StorePage(),
+    '/details_page': (context) => DetailsPage(),
+    '/cart_page': (context) => CartPage()
+  };
+}
