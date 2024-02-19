533854f97c484805f0e312fbf2cfd80dc829996c
Zyttal <calz483k@gmail.com>
2024-02-20 07:12:38 +0800

    Initialize Search Bar Widget - Function is still to be implemented

--- lib/pages/store_page.dart
+++ lib/pages/store_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pet_shop_application/widgets/custom_search_bar.dart';
 
 class StorePage extends StatelessWidget {
   const StorePage({super.key});
@@ -22,39 +23,7 @@ class StorePage extends StatelessWidget {
           SizedBox(
             height: 15,
           ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Theme.of(context).hintColor,
-              ),
-              child: Padding(
-                padding:
-                    EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Search Product or Brand",
-                            hintStyle: Theme.of(context).textTheme.bodyMedium,
-                            border: InputBorder.none),
-                        cursorColor: Theme.of(context).highlightColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          )
+          CustomSearchBar()
         ],
       )),
     );
---
+++ lib/widgets/custom_search_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatefulWidget {
+  const CustomSearchBar({
+    super.key,
+  });
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Theme.of(context).hintColor,
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(width: 10),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Search Product or Brand",
+                      hintStyle: Theme.of(context).textTheme.bodyMedium,
+                      border: InputBorder.none),
+                  cursorColor: Theme.of(context).highlightColor,
+                  onChanged: _handleSearch,
+                ),
+              ),
+              Icon(
+                Icons.search,
+                size: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _handleSearch(String input) {}
