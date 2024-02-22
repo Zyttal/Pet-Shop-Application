464f43ced75b3162b417e7c70d1e8a5536a6e242
Zyttal <calz483k@gmail.com>
2024-02-22 20:54:24 +0800

    Add List of Cart Contents - Pets added from the Catalog can now be seen in the Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pet_shop_application/providers/cart_provider.dart';
 import 'package:pet_shop_application/providers/navigation_provider.dart';
 import 'package:pet_shop_application/widgets/bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
@@ -9,9 +10,28 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final navigationProvider = Provider.of<NavigationProvider>(context);
-
+    final cartProvider = Provider.of<CartProvider>(context);
     return Scaffold(
-      body: Column(children: []),
+      body: Column(children: [
+        ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: cartProvider.pets.length,
+          itemBuilder: (context, index) {
+            final pet = cartProvider.pets[index];
+            return ListTile(
+                leading: Image.network(
+                  pet.peekImageURL,
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                ),
+                title: Text(pet.breed),
+                subtitle: Text('${pet.weight} kg - ${pet.height} cm'),
+                trailing: Text('\$${pet.price.toStringAsFixed(2)}'));
+          },
+        )
+      ]),
       bottomNavigationBar: BottomNavBar(navigationProvider: navigationProvider),
     );
   }
