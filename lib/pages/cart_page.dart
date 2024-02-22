5a21ab69f224207a01018d3feac584e3906c1f8d
Zyttal <calz483k@gmail.com>
2024-02-22 21:01:01 +0800

    Add remove item in Cart page - Above the Pricing of the content, a Delete button is added

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pet_shop_application/mock_data.dart';
 import 'package:pet_shop_application/providers/cart_provider.dart';
 import 'package:pet_shop_application/providers/navigation_provider.dart';
 import 'package:pet_shop_application/widgets/bottom_nav_bar.dart';
@@ -12,26 +13,46 @@ class CartPage extends StatelessWidget {
     final navigationProvider = Provider.of<NavigationProvider>(context);
     final cartProvider = Provider.of<CartProvider>(context);
     return Scaffold(
-      body: Column(children: [
-        ListView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          itemCount: cartProvider.pets.length,
-          itemBuilder: (context, index) {
-            final pet = cartProvider.pets[index];
-            return ListTile(
-                leading: Image.network(
-                  pet.peekImageURL,
-                  height: 50,
-                  width: 50,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(pet.breed),
-                subtitle: Text('${pet.weight} kg - ${pet.height} cm'),
-                trailing: Text('\$${pet.price.toStringAsFixed(2)}'));
-          },
-        )
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: cartProvider.pets.length,
+            itemBuilder: (context, index) {
+              final pet = cartProvider.pets[index];
+              final petStore = PetStoreList.firstWhere(
+                  (store) => store.ID == pet.petStoreID);
+              return ListTile(
+                  leading: Image.network(
+                    pet.peekImageURL,
+                    height: 50,
+                    width: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(pet.breed),
+                  subtitle: Text('${petStore.name}'),
+                  trailing: Column(
+                    children: [
+                      InkWell(
+                        child: Icon(Icons.delete_outline_sharp),
+                        onTap: () => {cartProvider.remove(pet)},
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        '\$${pet.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            color: Theme.of(context).highlightColor,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ));
+            },
+          )
+        ]),
+      ),
       bottomNavigationBar: BottomNavBar(navigationProvider: navigationProvider),
     );
   }
