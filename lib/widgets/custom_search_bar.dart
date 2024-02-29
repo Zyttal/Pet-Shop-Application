9f6956f724ea83f1c88f7afb8f545eaf16815c2f
Zyttal <calz483k@gmail.com>
2024-02-29 13:20:53 +0800

    Add Dependencies - Removed an import in one of the widgets - Added Riverpod package and related dependencies

--- lib/widgets/custom_search_bar.dart
+++ lib/widgets/custom_search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_shop_application/mock_data.dart';
 import 'package:pet_shop_application/pages/details_page.dart';
 
